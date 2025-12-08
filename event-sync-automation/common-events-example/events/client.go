50a18e506852fe95663eb4527834a9eded5ece98
zhouzicheng <zi-cheng.zhou@hp.com>
2025-12-08 09:43:11 +0800

    feat：test

--- event-sync-automation/common-events-example/events/client.go
+++ event-sync-automation/common-events-example/events/client.go
@@ -4,13 +4,13 @@ import "time"
 
 // ClientKVMSessionRequestedEvent represents a client KVM session request event
 type ClientKVMSessionRequestedEvent struct {
-    //EventID   string    `json:"event_id"`
-    //EventName string    `json:"event_name"`
-    //EventTime time.Time `json:"event_time"`
-    //ClientID  string    `json:"client_id"`
-    //DeviceID  string    `json:"device_id"`
-    //AccountID string    `json:"account_id"`
-    SessionID string `json:"session_id"`
+    EventID   string    `json:"event_id"`
+    EventName string    `json:"event_name"`
+    EventTime time.Time `json:"event_time"`
+    ClientID  string    `json:"client_id"`
+    DeviceID  string    `json:"device_id"`
+    AccountID string    `json:"account_id"`
+    SessionID string    `json:"session_id"`
 }
 
 // ClientKVMSessionCloseEvent represents a client KVM session close event
